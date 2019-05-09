c5037e5b7942ece03f63444bcc5ebfdcc6726459
Uriel Carneiro <urielhk17@gmail.com>
2019-05-09 12:35:48 -0400

    add status EM ANDAMENTO

--- resources/js/components/TicketStatus.vue
+++ resources/js/components/TicketStatus.vue
@@ -2,6 +2,7 @@
     <v-chip color="green" text-color="white" v-if="status == 'RESOLVIDO'">{{ status }}</v-chip>
     <v-chip color="warning" text-color="white" v-else-if="status == 'PENDENTE'">{{ status }}</v-chip>
     <v-chip color="red" text-color="white" v-else-if="status == 'NÃO RESOLVIDO'">{{ status }}</v-chip>
+    <v-chip color="blue" text-color="white" v-else-if="status == 'EM ANDAMENTO'">{{ status }}</v-chip>
 </template>
 
 <script>
