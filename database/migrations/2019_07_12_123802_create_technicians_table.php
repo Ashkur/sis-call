880931689a4508b85b34bf4ebb2afd12b3fc73cf
Uriel Carneiro <urielhk17@gmail.com>
2019-07-12 08:47:20 -0400

    gitflow-feature-stash: novas_tabelas
    Adicionado as tabelas Technicians, Warnings e alteração da tabela Tickets.

---
+++ database/migrations/2019_07_12_123802_create_technicians_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTechniciansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('technicians', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->unsignedBigInteger('department_id');
+            $table->timestamps();
+
+            $table->foreign('department_id')
+                ->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('technicians');
+    }
+}
---
+++ database/migrations/2019_07_12_123932_create_warnings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWarningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warnings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('id_technician');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('id_technician')
+                ->references('id')->on('technicians');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warnings');
+    }
+}
